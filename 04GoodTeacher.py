4c8e6498d9acfce4ba32c497bc5673244c65b2b1
LongWhiskers6 <vicky.shao@education.nsw.gov.au>
2021-11-02 00:26:27 +0000

    sdxfcgvhjbn

---
+++ 04GoodTeacher.py
@@ -0,0 +1,12 @@
+print (" ------------------------------------------------")
+print ("|                                               |")
+print ("|    04GoodTeacher                              |")
+print ("|    Name : Vicky                               |")
+print ("|    Version : 01                               |")
+print ("|                                               |")
+print (" ------------------------------------------------")
+x = input("Tacher's name (Try Mr Horan) ")
+if Mr Horan:
+    print ("You are lucky, he is a great teacher.")
+else:
+    print (x+"is an ok teacher")
