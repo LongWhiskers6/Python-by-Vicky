92866837ec8b2bc69aebdea2c834f33cb3e54862
LongWhiskers6 <vicky.shao@education.nsw.gov.au>
2021-11-03 00:43:47 +0000

    vbnm

---
+++ 05ForLoop.py
@@ -0,0 +1,9 @@
+print (" ------------------------------------------------")
+print ("|                                               |")
+print ("|    05ForLoop                                  |")
+print ("|    Name : Vicky                               |")
+print ("|    Version : 01                               |")
+print ("|                                               |")
+print (" ------------------------------------------------")
+for x in range(1, 500):
+  print(x)
