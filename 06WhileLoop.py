e6651e09556689bf75247f9e015236b6307570ea
LongWhiskers6 <vicky.shao@education.nsw.gov.au>
2021-11-05 05:19:07 +0000

    gvvhbjnk

---
+++ 06WhileLoop.py
@@ -0,0 +1,14 @@
+print (" ------------------------------------------------")
+print ("|                                               |")
+print ("|    06WhileLoop                                |")
+print ("|    Name : Vicky                               |")
+print ("|    Version : 01                               |")
+print ("|                                               |")
+print (" ------------------------------------------------")
+x = input("What is the name of this subject ")
+while x != "IST":
+    print("Not Correct - try again")
+    x += 1
+else:
+    print(" ")
+    print("Congratualations")
